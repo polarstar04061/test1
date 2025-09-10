6b030e9e12708b9a92557b5062548998ac40e682
polarstar04061 <xiaoxin.star0406@gmail.com>
2025-09-10 15:41:45 +0800

    Update firebaseAdmin.js

--- helper/firebaseAdmin.js
+++ helper/firebaseAdmin.js
@@ -1,10 +1,10 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../config/firebase-service-account.json");
+const serviceAccount = require("../config/new.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
